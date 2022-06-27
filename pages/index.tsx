fa50147767b3ecc87e12e4bdef9147baa520f3f4
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-06-27 18:45:35 +0200

    Add meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,15 @@ import FixedBackgroundImage from "../components/shared/FixedBackgroundImage";
 import { Divider } from "@mui/material";
 
 const TITLE = "depor.travel - Viajar y hacer deporte";
+const DESCRIPTION =
+  "Multitud de viajes de carácter 100% deportivo. Si buscas practicar tus deportes favoritos en lugares nuevos, estás de enhorabuena.";
 
 function SqueezePage() {
   return (
     <>
       <Head>
         <title>{TITLE}</title>
+        <meta name="description" content={DESCRIPTION} />
         <link rel="canonical" href="https://depor.travel" />
       </Head>
       <CssBaseline />
