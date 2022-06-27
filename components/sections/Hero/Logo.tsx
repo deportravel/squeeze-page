88ec9277ecb939f66c0d884aead4d70737921bf7
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-06-27 18:05:31 +0200

    Make logo bold

--- components/sections/Hero/Logo.tsx
+++ components/sections/Hero/Logo.tsx
@@ -2,13 +2,19 @@ import { Typography } from "@mui/material";
 
 export default function Logo() {
   return (
-    <Typography variant="h1" gutterBottom fontSize={{ xs: 45, sm: 60 }}>
+    <Typography
+      variant="h1"
+      gutterBottom
+      fontSize={{ xs: 45, sm: 60 }}
+      fontWeight="bold"
+    >
       depor
       <Typography
         component="span"
         color="primary.main"
         variant="h1"
-        fontSize={{ xs: 75, sm: 90 }}
+        fontWeight="bold"
+        fontSize={{ xs: 70, sm: 80 }}
       >
         {`.`}
       </Typography>
