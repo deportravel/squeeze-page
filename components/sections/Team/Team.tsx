44dd04dc8e59e0c990219d54d109574f68e7145e
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-06-30 15:11:56 +0200

    Add pictures and LinkedIn profile URL for team members

--- components/sections/Team/Team.tsx
+++ components/sections/Team/Team.tsx
@@ -2,6 +2,7 @@ import {
   Avatar,
   Box,
   Container,
+  Link,
   List,
   ListItem,
   ListItemAvatar,
@@ -10,7 +11,10 @@ import {
 } from "@mui/material";
 import Image from "next/image";
 import Section from "../../shared/Section";
-import teamMemberImage from "./37551468_243058243186200_3316334310134382592_n.jpeg";
+import antonioTeamMemberImage from "./antonio.jpeg";
+import allieTeamMemberImage from "./allie.jpeg";
+import guillermoTeamMemberImage from "./guillermo.jpg";
+import LinkedInLogo from "./LinkedInLogo";
 
 const TEAM_HEADLINE = "¿Quiénes somos?";
 
@@ -18,14 +22,20 @@ const teamMembers = [
   {
     name: "Antonio Sanchis",
     role: "Product & Marketing",
+    image: antonioTeamMemberImage,
+    linkedInProfileUrl: "https://www.linkedin.com/in/antoniogonzalezsanchis/",
   },
   {
     name: "Ali DeCastro",
     role: "Design",
+    image: allieTeamMemberImage,
+    linkedInProfileUrl: "https://www.linkedin.com/in/allie-decastro-b26a9452/",
   },
   {
     name: "Guillermo de la Puente",
     role: "Technology",
+    image: guillermoTeamMemberImage,
+    linkedInProfileUrl: "https://www.linkedin.com/in/guillermodlpa/",
   },
 ];
 
@@ -41,6 +51,7 @@ export default function Team() {
           alignItems: "center",
           pt: { xs: 4, sm: 8 },
           pb: { xs: 6, sm: 12 },
+          px: 0,
         }}
       >
         <Box
@@ -57,18 +68,35 @@ export default function Team() {
 
           <List>
             {teamMembers.map((teamMember) => (
-              <ListItem key={teamMember.name}>
+              <ListItem
+                key={teamMember.name}
+                secondaryAction={
+                  <Link
+                    href={teamMember.linkedInProfileUrl}
+                    color="secondary"
+                    sx={{
+                      transition: "opacity 0.25s ease",
+                      opacity: 1,
+                      "&:hover": {
+                        opacity: 0.8,
+                      },
+                    }}
+                  >
+                    <LinkedInLogo />
+                  </Link>
+                }
+              >
                 <ListItemAvatar>
                   <Avatar>
                     <Image
-                      src={teamMemberImage}
+                      src={teamMember.image}
                       alt="Avatar"
                       layout="fill"
                       objectFit="cover"
                     />
                   </Avatar>
                 </ListItemAvatar>
-                <Box>
+                <Box mr={{ xs: 1, sm: 2 }}>
                   <ListItemText
                     primary={teamMember.name}
                     secondary={teamMember.role}
