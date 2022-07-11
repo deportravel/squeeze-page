ca5d41d26b959b868fd9e8fbb599ac52558ae1ba
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-07-11 23:28:51 +0200

    Add real quotes

--- components/sections/Quotes/Quotes.tsx
+++ components/sections/Quotes/Quotes.tsx
@@ -1,46 +1,58 @@
 import { Avatar, Box, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import Section from "../../shared/Section";
-import quoteAvatarImage from "./37551468_243058243186200_3316334310134382592_n.jpeg";
+import avatarImageGerardIzquierdo from "./avatar_gerard_izquierdo.png";
+import avatarImageRogerFornas from "./avatar_roger_fornas.png";
 
-const NAME = "Antonio S.";
-const QUOTE = `"Hace falta una página para encontrar viajes deportivos en todo el mundo y generar una comunidad."`;
+const QUOTES = [
+  {
+    name: "Gerard Izquierdo",
+    image: avatarImageGerardIzquierdo,
+    content:
+      "Soy un fanático de los deportes acuáticos (waterpolo, natación, vela, windsurf…) y me encanta viajar. Llevaba mucho tiempo esperando algo como depor.travel. ¡No puedo esperar a vuestro lanzamiento oficial!",
+  },
+  {
+    name: "Roger Fornàs",
+    image: avatarImageRogerFornas,
+    content:
+      "He visitado varios países para hacer rutas de montaña, ya sea andando, ascendiendo a picos, corriendo o con bici de montaña. Tardaba semanas hasta encontrar opciones interesantes. ¿Dónde estaba depor.travel hasta ahora?",
+  },
+];
 
 export default function Quotes() {
   return (
     <Section backgroundColor={"background.shaded"}>
       <Container
-        maxWidth="sm"
+        maxWidth="lg"
         sx={{
           position: "relative",
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: "flex-start",
           pt: { xs: 4, sm: 8 },
           pb: { xs: 6, sm: 12 },
+          gap: { xs: 6, sm: 8 },
         }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: 4,
-          }}
-        >
-          <Avatar sx={{ width: "6rem", height: "6rem" }}>
-            <Image
-              src={quoteAvatarImage}
-              alt="Avatar"
-              layout="fill"
-              objectFit="cover"
-            />
-          </Avatar>
-          <Typography variant="h5" component="p">
-            {NAME}
-          </Typography>
-          <Typography variant="body1">{QUOTE}</Typography>
-        </Box>
+        {QUOTES.map(({ name, content, image }) => (
+          <Box
+            key={name}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <Avatar sx={{ width: "8rem", height: "8rem" }}>
+              <Image src={image} alt="Avatar" layout="fill" objectFit="cover" />
+            </Avatar>
+            <Typography variant="subtitle1" component="p" gutterBottom>
+              {name}
+            </Typography>
+            <Typography variant="body1">{`"${content}"`}</Typography>
+          </Box>
+        ))}
       </Container>
     </Section>
   );
